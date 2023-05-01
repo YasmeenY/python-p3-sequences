ac7d0c86196a9f2a4a348726176b9a7f2ecfbe9b
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-05-01 15:19:40 -0400

    passing tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = list()
+    n1,n2 = 0,1
+    count = 0
+    while (count < length):
+        fib_list.append(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        count += 1
+    print(fib_list)
